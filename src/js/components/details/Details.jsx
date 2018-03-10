797e3fcc5bd57e3ec4dfd32bb820f73be039a0d8
Erik Allison <erikallison23@gmail.com>
2018-03-09 20:10:13 -0800

    style and details page

--- src/js/components/details/Details.jsx
+++ src/js/components/details/Details.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import {
     details,
@@ -10,29 +10,61 @@ import {
 class Details extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.handleChangeDetails = this.handleChangeDetails.bind(this);
         this.handleDetailsBtn = this.handleDetailsBtn.bind(this);
     }
 
-    handleChangeDetails(e){
+    handleChangeDetails(e) {
         let input = e.target.value;
-        const {dispatch} = this.props;
+        const { dispatch } = this.props;
         dispatch(details(input));
     }
 
-    handleDetailsBtn(e){
-        const {dispatch} = this.props;
+    handleDetailsBtn(e) {
+        const { dispatch } = this.props;
         dispatch(detailsBtn());
     }
-    
+
     render() {
 
         return (
             <div>
-                <h1>Hello Details {this.props.input}</h1>
-                <input type="text" onChange={this.handleChangeDetails}/>
-                <button type="button" onClick={this.handleDetailsBtn}>Change</button>
+
+                <div className='container' id='EdetailsContainer'>
+                <h1> Easy Street Sign </h1>
+                    <hr/>
+                    <div className='row' id='EdetailsRow'>
+                  
+                        <div className = 'col-md-4' id='EdetailsCol4'>   
+                            <div className = 'card' id='col4Card'> 
+                                <img className ='EdetailsImg' src= 'http://ronedmondson.com/wp-content/uploads/2012/01/easy-street.jpg'   />
+                            </div>
+                        </div>
+
+                        <div className = 'col-md-8' id='EdetailsCol8'>
+
+                            <div className = 'card' id='col8Card'>
+                                <span>Price: $69</span>
+                            </div>
+
+                        <div className = 'card' id='col8Card'>
+                                <span>Qty: <input className='EdetailsInput' type='number'></input>  </span>
+                                </div>
+
+                        <div className = 'card' id='col8Card'>
+                                <p>Description: This easy street sign is one of a kind. You will always be on easy street forever! </p>
+                                </div>
+
+                       <div className = 'card' id='col8Card'>
+                                <p>Details: height-69cm width-420cm weight-4.20lbs shipping-free!</p>
+                                </div>         
+                            
+                            <button className='EdetailsBtn'>Add to cart</button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
