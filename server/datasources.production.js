29ec5dfa536c0c99c3f332ced1d4e12a90074934
Devin Ledesma <dxangobiz@gmail.com>
2018-03-13 22:44:48 -0700

    Broke down mongodb production source

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,7 +1,14 @@
 module.exports = {
     "MongoDB": {
+        "host": "ds113749.mlab.com",
+        "port": 13749,
+        "url": "",
+        "database": "heroku_jx8q9hmz",
+        "password": "f012dghmgtqp230gobb362epcm",
         "name": "MongoDB",
-        "connector": "mongodb",
-        "url": process.env.MONGODB_URI
-    }
+        "user": "heroku_jx8q9hmz",
+        "connector": "mongodb"
+    }      
 }
+
+//mongodb://heroku_jx8q9hmz:f012dghmgtqp230gobb362epcm@ds113749.mlab.com:13749/heroku_jx8q9hmz
